71dd52c44cd1d8fc24cb6e318c820441bfe52b84
andreasbachmann <github.prolonged@turinturambar.com>
2026-02-04 16:29:51 +0100

    keep peptides used by ebm column

--- R/deduplicate.R
+++ R/deduplicate.R
@@ -4,7 +4,7 @@ deduplicate_results <- function(results, global_mode = FALSE) {
   if (global_mode) {
     # global mode
     deduped <- results %>%
-      group_by(Label, mean_log2FC, median_log2FC, adj_p_right, adj_p_left, n_peptides) %>%
+      group_by(Label, mean_log2FC, median_log2FC, adj_p_right, adj_p_left, n_peptides, n_peptides_used) %>%
       slice_min(nchar(DomainName), n = 1, with_ties = FALSE) %>%
       ungroup() %>%
       select(
@@ -15,7 +15,7 @@ deduplicate_results <- function(results, global_mode = FALSE) {
   } else {
     # instance mode
     deduped <- results %>%
-      group_by(ProteinName, Label, mean_log2FC, median_log2FC, adj_p_right, adj_p_left, n_peptides) %>%
+      group_by(ProteinName, Label, mean_log2FC, median_log2FC, adj_p_right, adj_p_left, n_peptides, n_peptides_used) %>%
       slice_min(nchar(DomainName), n = 1, with_ties = FALSE) %>%
       ungroup() %>%
       select(
