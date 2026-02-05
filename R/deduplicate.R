400b9b9ef2d71b3a71bff156098e21286fd038b3
andreasbachmann <github.prolonged@turinturambar.com>
2026-02-05 10:47:28 +0100

    column arrangement change

--- R/deduplicate.R
+++ R/deduplicate.R
@@ -4,23 +4,22 @@ deduplicate_results <- function(results, global_mode = FALSE) {
   if (global_mode) {
     # global mode
     deduped <- results %>%
-      group_by(Label, mean_log2FC, median_log2FC, adj_p_right, adj_p_left, n_peptides) %>%
+      group_by(Label, mean_log2FC, median_log2FC, adj_p_left, adj_p_right, n_peptides) %>%
       slice_min(nchar(DomainName), n = 1, with_ties = FALSE) %>%
       ungroup() %>%
       select(
-        DomainName, ProteinName, Label, n_peptides,
-        adj_p_right, adj_p_left, mean_log2FC, median_log2FC, method,
-        n_proteins, n_peptides_used, n_domain_instances
+        DomainName, ProteinName, Label, n_proteins, n_peptides, n_peptides_used, n_domain_instances, adj_p_left, 
+        adj_p_right, mean_log2FC, median_log2FC, method
       )
   } else {
     # instance mode
     deduped <- results %>%
-      group_by(ProteinName, Label, mean_log2FC, median_log2FC, adj_p_right, adj_p_left, n_peptides) %>%
+      group_by(ProteinName, Label, mean_log2FC, median_log2FC, adj_p_left, adj_p_right, n_peptides) %>%
       slice_min(nchar(DomainName), n = 1, with_ties = FALSE) %>%
       ungroup() %>%
       select(
         DomainID, DomainName, ProteinName, Label, n_peptides, n_peptides_used,
-        adj_p_right, adj_p_left, mean_log2FC, median_log2FC, method
+        adj_p_left, adj_p_right, mean_log2FC, median_log2FC, method
       )
   }
 
